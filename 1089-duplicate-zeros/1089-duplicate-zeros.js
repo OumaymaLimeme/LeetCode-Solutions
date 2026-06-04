e8474b3aca852814887642143dd4595cf5c4596f
OumaymaLimeme <92635384+OumaymaLimeme@users.noreply.github.com>
2026-06-04 22:07:21 +0100

    Time: 2 ms (72.62%), Space: 58.4 MB (14.88%) - LeetHub

---
+++ 1089-duplicate-zeros/1089-duplicate-zeros.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} arr
+ * @return {void} Do not return anything, modify arr in-place instead.
+ */
+var duplicateZeros = function (arr) {
+    let res = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        res.push(arr[i]);
+
+        if (arr[i] === 0) {
+            res.push(0);
+        }
+    }
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = res[i];
+    }
+
+};
